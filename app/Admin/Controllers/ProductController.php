82fcc915f6d9623e4ae64f4603cce263719c9a5e
dienducminh <dienducminh2201@gmail.com>
2024-02-20 17:27:33 +0700

    first commit

---
+++ app/Admin/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Product;
+
+class ProductController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Product';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('category', __('Category'));
+        $grid->column('image')->image();
+        $grid->column('price', __('Price'));
+        $grid->column('stock', __('Stock'));
+        $grid->column('rateing', __('Rateing'));
+        $grid->column('Material', __('Material'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('category', __('Category'));
+        $show->field('image', __('Image'));
+        $show->field('price', __('Price'));
+        $show->field('stock', __('Stock'));
+        $show->field('rateing', __('Rateing'));
+        $show->field('Material', __('Material'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product());
+
+        $form->text('name', __('Name'));
+        $form->text('category', __('Category'));
+        $form->image('image', __('Image'));
+        $form->decimal('price', __('Price'));
+        $form->number('stock', __('Stock'));
+        $form->text('rateing', __('Rateing'));
+        $form->text('Material', __('Material'));
+
+        return $form;
+    }
+}
---
+++ database/migrations/2024_02_20_034748_create_Product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->double('price')->nullable();
+            $table->integer('stock')->nullable();
+            $table->string('rateing')->nullable();
+            $table->string('Material')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Product');
+    }
+}
